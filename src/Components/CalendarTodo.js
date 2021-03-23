69cf1d2dbb9e00b1c44aed20013f707400c0bb58
mustafa cagatay kaban <cagataykaban@mustafas-MacBook-Pro.local>
2021-03-23 11:25:33 +0300

    completed calendar

--- src/Components/CalendarTodo.js
+++ src/Components/CalendarTodo.js
@@ -1,60 +1,43 @@
 import { Calendar, Badge } from "antd";
 import "./CalendarTodo.css";
-const CalendarTodo = (props) => {
-function getListData(value) {
-  let listData;
-  for(let i=0; i <= todos.length; i++){
-      let todo = todos[i]
-      let dayOfTodo = new Date(todos[i]?.date).toLocaleDateString('tr-TR').substring(0,2)
-      dayOfTodo = parseInt(dayOfTodo)
-    //   let monthOfTodo = new Date(todos[i]?.date).toLocaleDateString('tr-TR').substring(3,5);
 
-    if (value.date() === dayOfTodo) {
-        listData = [
-            { type: "warning", content: todo.title },
-          ];
-        }
+const CalendarTodo = (props) => {
+  function getListData(value) {
+    let listData = [];
+    for (let i = 0; i <= todos.length; i++) {
+      let todo = todos[i];
+      let dateOfTodo = new Date(todos[i]?.date).toLocaleDateString("tr-TR");
+      let dayOfTodo = dateOfTodo.substring(0, 2);
+      dayOfTodo = parseInt(dayOfTodo);
+      let monthOfTodo = parseInt(dateOfTodo.substring(3, 5));
+      let yearOfTodo = parseInt(dateOfTodo.substr(6, 10));
+      let newCalendarTodo = { type: "warning", content: todo?.title };
+      if (
+        value.date() === dayOfTodo &&
+        value.month() === monthOfTodo - 1 &&
+        value.year() === yearOfTodo
+      ) {
+        listData.push(newCalendarTodo);
+      }
     }
     return listData || [];
-}
-
-function dateCellRender(value) {
-  const listData = getListData(value);
-  return (
-    <ul className="events">
-      {listData.map((item) => (
-        <li key={item.content}>
-          <Badge status={item.type} text={item.content} />
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-function getMonthData(value) {
-  if (value.month() === 8) {
-    return 1394;
   }
-}
 
-function monthCellRender(value) {
-  const num = getMonthData(value);
-  return num ? (
-    <div className="notes-month">
-      <section>{num}</section>
-      <span>Backlog number</span>
-    </div>
-  ) : null;
-}
+  function dateCellRender(value) {
+    const listData = getListData(value);
+    return (
+      <ul className="events">
+        {listData.map((item) => (
+          <li key={item.content}>
+            <Badge status={item.type} text={item.content} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
 
   const { todos } = props;
-    console.log(new Date(todos[0]?.date).toLocaleDateString('tr-TR').substring(0,2))
-  return (
-    <Calendar
-      dateCellRender={dateCellRender}
-      monthCellRender={monthCellRender}
-    />
-  );
+  return <Calendar dateCellRender={dateCellRender} />;
 };
 
 export default CalendarTodo;
